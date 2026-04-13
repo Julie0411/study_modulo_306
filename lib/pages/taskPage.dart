486ccea8f7e492db934b9c52ddde706ad51716b8
Julie <julie.gambale@gmail.com>
2026-04-13 14:05:26 +0200

    added task card and widget

--- lib/pages/taskPage.dart
+++ lib/pages/taskPage.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import '../widgets/placeholderCard.dart';
+import '../widgets/taskCard.dart';
 
 class TasksPage extends StatelessWidget {
   const TasksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PlaceholderCard(title: 'Tasks');
+    return const taskCard(title: 'Tasks');
   }
 }
---
+++ lib/widgets/taskCard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class taskCard extends StatelessWidget {
+  final String title;
+
+  const taskCard({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.white54,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.deepPurple.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: const Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.deepPurpleAccent[700],
+          ),
+        ),
+      ),
+    );
+  }
+}
